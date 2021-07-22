f51f98b791fe7db32bdd31478c626c03d28e4b0f
arminpashankar <87486743+arminpashankar@users.noreply.github.com>
2021-07-22 15:33:48 +0530

    Update Test2.java
    Added logic of sum

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,5 +2,14 @@ public class Test2 {
     //another test case
     public static void main(String[] args) {
         System.out.println("Added std messages.");
+        
+        //Armin added code
+        int a = 10;
+        int b = 20;
+        int sum;
+        sum = a + b;
+        System.out.println("sum =" + sum);
+        
+     
     }
 }
